0e77ef1fbdf0bd5a3243696684eea55c7fad697c
Hojinida <opqrs1103@gmail.com>
2023-03-28 21:55:05 +0900

    project start

---
+++ src/test/java/com/project/carpool/CarpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.carpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
