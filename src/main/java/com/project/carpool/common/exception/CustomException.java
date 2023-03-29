8f862e67418a32c3332626bdef947ac1fd648870
Hojinida <opqrs1103@gmail.com>
2023-03-29 11:09:31 +0900

    feat: CustomException Class

---
+++ src/main/java/com/project/carpool/common/exception/CustomException.java
@@ -0,0 +1,12 @@
+package com.project.carpool.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException {
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode){
+        this.errorCode=errorCode;
+    }
+}
