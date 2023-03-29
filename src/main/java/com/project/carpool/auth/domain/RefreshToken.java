a2c1542752fec014f9dc32975ceedda1e5845ad6
Hojinida <opqrs1103@gmail.com>
2023-03-29 13:31:58 +0900

    feat: RefreshTokenRepository interface

--- src/main/java/com/project/carpool/auth/domain/RefreshToken.java
+++ src/main/java/com/project/carpool/auth/domain/RefreshToken.java
@@ -1,4 +1,23 @@
 package com.project.carpool.auth.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@RequiredArgsConstructor
+@RedisHash(value = "refreshToken", timeToLive = 60)
 public class RefreshToken {
+    @Id
+    @Indexed
+    private String key;
+    private Long value;
+    @Builder
+    public RefreshToken(final String key, final Long value) {
+        this.key = key;
+        this.value = value;
+    }
 }
